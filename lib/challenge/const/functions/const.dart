add4df3e25d6bc4e86942b4cc537b3b2672e10da
mohamedahemd99 <58490629+mohamedahemd99@users.noreply.github.com>
2022-03-08 12:57:36 -0800

    Map instead of list

--- lib/challenge/const/functions/const.dart
+++ lib/challenge/const/functions/const.dart
@@ -2,3 +2,18 @@ List<int> orders = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 String image =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFzhsnlVVQ1PCnMjdoBqFtQJILUpyPKVEGqg&usqp=CAU";
+Map<String, int>orders1={
+  "jan":0,
+  "feb":0,
+  "march":0,
+  "apr":0,
+  "may":0,
+  "jun":0,
+  "jul":0,
+  "aug":0,
+  "sep":0,
+  "oct":0,
+  "nov":0,
+  "dec":0,
+};
+String? month;
